031507a5d405e1ec606395dd0f8372a574387563
usertomR <ryusuke.tomita.engineer@gmail.com>
2021-11-03 18:10:21 +0900

    make skeleton of pages & static 4 pages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+
+    #ページ毎の完全なタイトルを返す。
+    def full_title(page_title = '')
+        base_title = "UExpress"
+        if base_title.empty?
+            base_title
+        else
+            page_title + " | " + base_title
+        end
+    end
 end
