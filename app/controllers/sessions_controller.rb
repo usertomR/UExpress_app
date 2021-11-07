1d748344b041c7977aed0d29c33f43207a26b8bc
usertomR <ryusuke.tomita.engineer@gmail.com>
2021-11-07 16:57:17 +0900

    Add a method log_in & current_user

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,8 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
-
+      log_in user
+      redirect_to user  #とりあえずuser_url(user)にしておく
     else
       flash.now[:danger] = 'どちらかまたは両方の入力を間違えています'
       render 'new'
