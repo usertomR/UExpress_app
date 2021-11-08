6251323328d34424c5df451e43e880728c9925fe
usertomR <ryusuke.tomita.engineer@gmail.com>
2021-11-08 18:06:54 +0900

    Can delete permanent login

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -18,7 +18,7 @@ module SessionsHelper
             @current_user ||= User.find_by(id: session[:user_id])
         elsif(user_id = cookies.signed[:user_id])
             user = User.find_by(id: user_id)
-            if user && user.authenticated?(cookies.[:remember_token])
+            if user && user.authenticated?(cookies[:remember_token])
                 log_in (user)
                 @current_user = user
             end
@@ -30,8 +30,16 @@ module SessionsHelper
         !current_user.nil?
     end
 
+    #永続的セッションを破棄する
+    def forget(user)
+        user.forget
+        cookies.delete(:user_id)
+        cookies.delete(:remember_token)
+    end
+
     #現在のユーザーをログアウトする
     def log_out
+        forget(current_user)
         session.delete(:user_id)
         @current_user = nil
     end
--- app/models/user.rb
+++ app/models/user.rb
@@ -29,7 +29,13 @@ class User < ApplicationRecord
     end
 
     def authenticated?(remember_token)
+        return false if remember_digest.nil?
         BCrypt::Password.new(remember_digest).is_password?(remember_token)
+    end
+
+    #ユーザーのログイン情報を破棄する
+    def forget
+        update_attribute(:remember_digest, nil)
     end
 
         private
