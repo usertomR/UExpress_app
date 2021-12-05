e5e14f42b68179a3f454b567a01c699335b232ec
usertomR <ryusuke.tomita.engineer@gmail.com>
2021-12-05 16:57:15 +0900

    (4th time)Refactor user_spec.rb #3

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -32,63 +32,62 @@ RSpec.describe User, type: :model do
       end
     end
 
-    it "is invalid without a email" do
-      user = User.new(email: "  ")
-      user.valid?
-      expect(user.errors[:email]).to include("can't be blank")
-    end
+    context ":email validation" do
+      it "is invalid without a email" do
+        user = User.new(email: "  ")
+        user.valid?
+        expect(user.errors[:email]).to include("can't be blank")
+      end
 
-  
+      it "has not too long email" do
+        user = User.new(email: "a" * 244 + "@example.com")
+        user.valid?
+        expect(user.errors[:email]).to include("is too long (maximum is 255 characters)")
+      end
 
-    it "has not too long email" do
-      user = User.new(email: "a" * 244 + "@example.com")
-      user.valid?
-      expect(user.errors[:email]).to include("is too long (maximum is 255 characters)")
-    end
+      it "accept vaild addresses if email is vaild" do
+        valid_addresses = %w[user@example.com USER@foo.com A_US-ER@foo.bar.org first.last@foo.jp
+                            alice+bob@baz.cn]
+        valid_addresses.each do |valid_address|
+          user = User.new(name: "Tom", email: "Tom@email.com", password: "Testuser",
+                          password_confirmation: "Testuser")
+          user.email = valid_address
+          expect(user).to be_valid
+        end
+      end
+    
+      it "is invalid with a duplicate email address" do
+        user = User.new(name: "Mom", email: "Tester1@email.com", password: "Tester", password_confirmation: "Tester")
+        user.valid?
+        expect(user.errors[:email]).to include("has already been taken")
+      end
 
-    it "accept vaild addresses if email is vaild" do
-      valid_addresses = %w[user@example.com USER@foo.com A_US-ER@foo.bar.org first.last@foo.jp
-                          alice+bob@baz.cn]
-      valid_addresses.each do |valid_address|
-        user = User.new(name: "Tom", email: "Tom@email.com", password: "Testuser",
-                        password_confirmation: "Testuser")
-        user.email = valid_address
-        expect(user).to be_valid
+      it "is invalid with a duplicate email address" do
+        User.create(name: "Tom", email: "Tom@email.com", password: "Testuser",
+          password_confirmation: "Testuser")
+        user = User.new(name: "Mom", email: "Tom@email.com", password: "Tester",
+          password_confirmation: "Tester")
+        user.valid?
+        expect(user.errors[:email]).to include("has already been taken")
       end
     end
-  
-    it "is invalid with a duplicate email address" do
-      user = User.new(name: "Mom", email: "Tester1@email.com", password: "Tester", password_confirmation: "Tester")
-      user.valid?
-      expect(user.errors[:email]).to include("has already been taken")
+
+    context ":password validation" do
+      it "is invalid with a password that is only composed of space key" do
+        user = User.new(name: "Tom", email: "Tom@email.com", password: " " * 6,
+                        password_confirmation: " " * 6)
+        user.valid?
+        expect(user.errors[:password]).to include("can't be blank")
+      end
+
+      it "is invslid with a password that is less than 6-length" do
+        user = User.new(name: "Tom", email: "Tom@email.com", password: "Testuser",
+                      password_confirmation: "Testuser")
+        user.password = user.password_confirmation = "a" * 5
+        user.valid?
+        expect(user.errors.full_messages).to include("Password is too short (minimum is 6 characters)")
+      end
     end
-  end
-  
-
-  it "is invalid with a duplicate email address" do
-    User.create(name: "Tom", email: "Tom@email.com", password: "Testuser",
-      password_confirmation: "Testuser")
-    user = User.new(name: "Mom", email: "Tom@email.com", password: "Tester",
-      password_confirmation: "Tester")
-    user.valid?
-    expect(user.errors[:email]).to include("has already been taken")
-  end
-
-  it "is invalid with a password that is only composed of space key" do
-    user = User.new(name: "Tom", email: "Tom@email.com", password: " " * 6,
-                    password_confirmation: " " * 6)
-    user.valid?
-    expect(user.errors[:password]).to include("can't be blank")
-  end
-
-  it "is invslid with a password that is less than 6-length" do
-    user = User.new(name: "Tom", email: "Tom@email.com", password: "Testuser",
-                  password_confirmation: "Testuser")
-    user.password = user.password_confirmation = "a" * 5
-    user.valid?
-    expect(user.errors.full_messages).to include(
-      "Password is too short (minimum is 6 characters)"
-    )
   end
 
   it "has a valid factory" do
@@ -98,6 +97,6 @@ RSpec.describe User, type: :model do
   it "has method[authenticated?] that return false for a user with nil digest" do
     user = User.new(name: "Tester", email: "Tester1@email.com", password: "Testuser",
       password_confirmation: "Testuser")
-    expect(user.authenticated?(:remember, '')).to eq false
+      expect(user.authenticated?(:remember, '')).to eq false
   end
 end
