833020928a66ee9d49f9cc35808b0e92f1d0736a
usertomR <ryusuke.tomita.engineer@gmail.com>
2021-12-17 00:07:00 +0900

    create 3buttons at root_path  #5

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,7 +9,7 @@ require("@rails/activestorage").start()
 require("channels")
 //= require jquery3
 //= require popper
-//= require bootstrap-sprockets
+//= require bootstrap
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -56,6 +56,9 @@ Rails.application.configure do
   # Suppress logger output for asset requests.
   config.assets.quiet = true
 
+  # 動的コンパイルを有効化
+  config.assets.compile = true
+
   # Raises error for missing translations.
   # config.action_view.raise_on_missing_translations = true
 
