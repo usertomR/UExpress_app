648e83927a734289b86cc271cce80b29bd1a17df
usertomR <ryusuke.tomita.engineer@gmail.com>
2022-02-11 18:16:25 +0900

    systemspec(article)/about create&update an article #11

--- app/validators/target_validator.rb
+++ app/validators/target_validator.rb
@@ -1,7 +1,7 @@
 class TargetValidator < ActiveModel::Validator
   def validate(record)
     unless record.Eschool_level || record.JHschool_level || record.Hschool_level
-      record.errors[:target] << '小学生・中学生・高校生向けのどれか1つは選択して下さい'
+      record.errors[:target] << "can't be blank"
     end
   end
 end
