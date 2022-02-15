34286fd65fff61b149cc31fc2aec426f317fec63
usertomR <ryusuke.tomita.engineer@gmail.com>
2022-02-15 17:02:15 +0900

    create Relationship model and methods #15

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,15 @@ class User < ApplicationRecord
   mount_uploader :avatar, AvatarUploader
   has_many :articles, dependent: :destroy
   has_many :questions, dependent: :destroy
+  has_many :active_relationships, class_name: "Relationship",
+                                   foreign_key: "follower_id",
+                                   dependent: :destroy
+  has_many :passive_relationships, class_name: "Relationship",
+                                   foreign_key: "followed_id",
+                                   dependent: :destroy
+  has_many :following, through: :active_relationships, source: :followed
+  has_many :followers, through: :passive_relationships, source: :follower
+
   attr_accessor :remember_token, :activation_token, :reset_token
 
   before_save :downcase_email
@@ -70,6 +79,21 @@ class User < ApplicationRecord
     reset_sent_at < 2.hours.ago
   end
 
+  # 以下3つは、メソッド内でself(呼び出し元のインスタンス変数)を省略している.
+  # 以下3つが使える理由は、このファイルで、has_many :following・・・を記述しているから。
+  # ユーザーをフォローする
+  def follow(other_user)
+    following << other_user
+  end
+
+  # ユーザーをフォロー解除する
+  def unfollow(other_user)
+    active_relationships.find_by(followed_id: other_user.id).destroy
+  end
+
+  def following?(other_user)
+    following.include?(other_user)
+  end
         private
 
   # メールアドレスを全て小文字にする。DBによって大文字小文字を区別する物もある
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_12_085550) do
+ActiveRecord::Schema.define(version: 2022_02_15_031137) do
 
   create_table "action_text_rich_texts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb3", force: :cascade do |t|
     t.string "name", null: false
@@ -74,6 +74,16 @@ ActiveRecord::Schema.define(version: 2022_02_12_085550) do
     t.index ["title"], name: "index_questions_on_title", unique: true
     t.index ["user_id", "updated_at"], name: "index_questions_on_user_id_and_updated_at"
     t.index ["user_id"], name: "index_questions_on_user_id"
+  end
+
+  create_table "relationships", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb3", force: :cascade do |t|
+    t.integer "follower_id"
+    t.integer "followed_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["followed_id"], name: "index_relationships_on_followed_id"
+    t.index ["follower_id", "followed_id"], name: "index_relationships_on_follower_id_and_followed_id", unique: true
+    t.index ["follower_id"], name: "index_relationships_on_follower_id"
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb3", force: :cascade do |t|
