642212d8c4cbdde7d44cc2e8c8a1482a48f206ed
usertomR <ryusuke.tomita.engineer@gmail.com>
2022-02-28 15:18:29 +0900

    can use comment delete button #25

--- app/controllers/article_comments_controller.rb
+++ app/controllers/article_comments_controller.rb
@@ -1,6 +1,9 @@
 class ArticleCommentsController < ApplicationController
   before_action :logged_in_user
+  before_action :correct_user, only: [:destroy]
 
+  # updateがないのは、コメントのやり取りがおかしくなる可能性があるから。
+  # コメントを修正したいならもう一度コメントを作って追記する形にすると、話の流れがおかしくならない
   def create
     @article = Article.find(params[:article_comment][:article_id])
     @comment = @article.article_comments.build(comment_params)
@@ -13,11 +16,20 @@ class ArticleCommentsController < ApplicationController
   end
 
   def destroy
+    @article = @comment.article
+    @comment.destroy
+    flash[:info] = "コメントを削除しました"
+    redirect_to browsing_article_path(@article)
   end
 
   private
 
   def comment_params
     params.require(:article_comment).permit(:user_id, :article_id, :comment)
+  end
+
+  def correct_user
+    @comment = current_user.article_comments.find_by(id: params[:id])
+    redirect_to root_url if @comment.nil?
   end
 end
