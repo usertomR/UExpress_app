eaa6159767e62a4847eae7f43bcf57a7d1139999
usertomR <ryusuke.tomita.engineer@gmail.com>
2022-03-07 21:15:52 +0900

    you can use nice button #34

--- app/controllers/nice_to_answers_controller.rb
+++ app/controllers/nice_to_answers_controller.rb
@@ -1,9 +1,17 @@
-class CommentToAnswersController < ApplicationController
+class NiceToAnswersController < ApplicationController
   before_action :logged_in_user
 
   def create
+    @answer = AnswerToQuestion.find(params[:answer_to_question_id])
+    @answer.nice_to_answers.create(user_id: params[:user_id])
+    @question = @answer.question
+    redirect_to browsing_question_path(@question)
   end
 
   def destroy
+    @nice_answer = NiceToAnswer.find(params[:id])
+    @question = Question.find(params[:question_id])
+    @nice_answer.destroy
+    redirect_to browsing_question_path(@question)
   end
 end
