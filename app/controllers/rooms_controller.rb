3003450a6f86d3213959947696aa9317afe2c857
usertomR <ryusuke.tomita.engineer@gmail.com>
2022-03-20 21:41:52 +0900

    can access conversation page and created templete of this page  #40

---
+++ app/controllers/rooms_controller.rb
@@ -0,0 +1,18 @@
+class RoomsController < ApplicationController
+  def create
+    @room = Room.create
+    @sended_user_entry = Entry.create(room_id: @room.id, user_id: params[:sended_user_id])
+    @sending_user_entry = Entry.create(room_id: @room.id, user_id: params[:sending_user_id])
+    redirect_to room_path(@room)
+  end
+
+  def show
+    @room = Room.find(params[:id])
+    @messages = @room.messages
+    @message = Message.new
+    @entries = @room.entries
+  end
+
+  def index
+  end
+end
--- app/models/room.rb
+++ app/models/room.rb
@@ -1,4 +1,18 @@
 class Room < ApplicationRecord
   has_many :messages, dependent: :destroy
   has_many :entries, class_name: "Entry", dependent: :destroy
+
+  def self.room_id_present?(sended_user, sending_user)
+    sended_user_sum_entries = Entry.where(user_id: sended_user.id)
+    sending_user_sum_entries = Entry.where(user_id: sending_user.id)
+
+    sended_user_sum_entries.each do |sended_entry|
+      sending_user_sum_entries.each do |sending_entry|
+        if sended_entry.room_id == sending_entry.room_id
+          return sending_entry.room_id
+        end
+      end
+    end
+    return nil
+  end
 end
