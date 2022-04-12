a30f8ff052dc8a42f5e57cd3b1c00d39b04fda3d
usertomR <ryusuke.tomita.engineer@gmail.com>
2022-04-12 21:16:28 +0900

    modified&created file(s) for AWS ECS

---
+++ entrypoint.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+set -e
+
+# server.pidがあると不具合を起こすことがあるはず。
+rm -f /myapp/tmp/pids/server.pid
+
+# データベースとアセットのセットアップ
+bundle exec rails db:migrate:reset RAILS_ENV=production DISABLE_DATABASE_ENVIRONMENT_CHECK=1
+bundle exec rails db:seed RAILS_ENV=production
+bundle exec rails assets:precompile RAILS_ENV=production
+
+# Then exec the container's main process (what's set as CMD in the Dockerfile).
+exec "$@"
