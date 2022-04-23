d3fe9c903c5e781ab930220f3864321fdbad187b
usertomR <ryusuke.tomita.engineer@gmail.com>
2022-04-23 13:06:54 +0900

    try to deploy as intended

---
+++ app/javascript/packs/judgement_notPC.js
@@ -0,0 +1,5 @@
+if(navigator.userAgent.match(/(iPhone|iPad|iPod|Android)/i)){
+	// スマホ・タブレット（iOS・Android）の場合の処理を記述
+  const app_title = document.getElementsByClassName("heading")[0];
+  app_title.style.marginTop = '100px'
+}
