4620afaf5e5ff6c8c52895bc9686e7baf4c15fb1
usertomR <ryusuke.tomita.engineer@gmail.com>
2022-04-26 15:49:16 +0900

    modified a file not to delete data every time CI/CD is done #46

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,8 +5,8 @@ set -e
 rm -f /myapp/tmp/pids/server.pid
 
 # データベースとアセットのセットアップ
-bundle exec rails db:migrate:reset RAILS_ENV=production DISABLE_DATABASE_ENVIRONMENT_CHECK=1
-bundle exec rails db:seed RAILS_ENV=production
+# bundle exec rails db:migrate:reset RAILS_ENV=production DISABLE_DATABASE_ENVIRONMENT_CHECK=1
+# bundle exec rails db:seed RAILS_ENV=production
 bundle exec rails assets:precompile RAILS_ENV=production
 
 # Then exec the container's main process (what's set as CMD in the Dockerfile).
