eb843c5397fd4f2a26d36d4b44105646f0d9201e
usertomR <ryusuke.tomita.engineer@gmail.com>
2022-06-29 21:05:34 +0900

    test #52

--- config/puma.rb
+++ config/puma.rb
@@ -13,7 +13,7 @@ threads min_threads_count, max_threads_count
 
 # Specifies the `environment` that Puma will run in.
 #
-environment ENV.fetch("RAILS_ENV") { "development" }
+environment ENV.fetch("RAILS_ENV") { "production" }
 
 # Specifies the `pidfile` that Puma will use.
 # pidとは、process IDのこと。プロセスIDを指定する
