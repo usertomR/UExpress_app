8c30ac4b4c6ccb3367edc0d5fb55f82ad6cc6e4f
usertomR <ryusuke.tomita.engineer@gmail.com>
2022-06-29 16:39:41 +0900

    <production env>modified a file to use real-time dm #52

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -40,8 +40,9 @@ Rails.application.configure do
 
   # Mount Action Cable outside main process or domain.
   # config.action_cable.mount_path = nil
-  # config.action_cable.url = 'wss://example.com/cable'
-  # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
+  config.action_cable.disable_request_forgery_protection = true
+  config.action_cable.url = 'wss://www.portfolio-uexpress.net/cable'
+  config.action_cable.allowed_request_origins = ['http://www.portfolio-uexpress.net']
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
